d3d50c7aeb1b753d6be2270ec1858a7f4f337412
francobdq <fbdq.dev@gmail.com>
2021-04-25 21:58:13 -0300

    ahora si

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import requests
+import json
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 
@@ -15,11 +17,44 @@ logging.basicConfig(level=logging.DEBUG)
 logging.debug("Log habilitad!")
 
 
+
+
+
+def guardarActividades():
+    logging.debug("guardando json")
+    response = requests.get(url=' http://areco.gob.ar:9528/api/actividad/all')
+    response_dict = response.json()
+
+    
+    print(response_dict['data'][0])
+    with open('data.json', 'w') as file:
+        json.dump(response_dict, file, indent=4)
+
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    #guardarActividades()
     if request.method == 'POST':
+        # datos usuario
+        nombre = request.form['nombre-apellido-input']
+        dni = request.form['dni-input']
+        email = request.form['email-input']
+        direccion = request.form['direccion-input']
+        tel = request.form['tel-input']
+        # declaracion jurada
+        olfato = request.form['olfato']
+        gusto = request.form['gusto']
+        tos = request.form['tos']
+        garganta = request.form['garganta']
+        aire = request.form['aire']
 
-        return redirect(url_for('uploaded_file', filename=nombreOUT))
+
+
+        return "<ul><li>nombre: "+ nombre +"<li/><li>dni: "+ dni +"<li/><li>email: "+ email +"<li/><li>direccion: "+ direccion +"<li/><li>telefono: "+ tel +"<li/></ul> <br/><ul><li>olfato: "+ olfato +"<li/><li>gusto: "+ gusto +"<li/><li>tos: "+ tos +"<li/><li>garganta: "+ garganta +"<li/><li>aire: "+ aire +"<li/></ul>"
+
+
 
     return render_template("index.html")
 
